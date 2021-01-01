617a50ce2c50173c20c531be0ced3cd814330c41
maxzzy <73212216+maxzzy@users.noreply.github.com>
2021-01-02 02:09:49 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const axios = require("axios")
+const urls = ["https://mature-horse-settee.glitch.me"]
+setInterval(function() {
+            urls.forEach(url => {
+            axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
+        })
+    }, 60 * 1000);
