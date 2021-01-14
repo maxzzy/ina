2e09ddd35b2a603d5bbfd66ff9e1eb4a28ef94f5
maxzzy <73212216+maxzzy@users.noreply.github.com>
2021-01-15 00:21:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://mature-horse-settee.glitch.me"]
+const urls = ["https://noxious.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
